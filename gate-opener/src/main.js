4cec2557ceb229d5fdcec17f173d2d20c164845c
Szymon Jankowski <s.jankowski@fireup.pro>
2021-09-16 15:57:31 +0200

    Improved a bit

--- gate-opener/src/main.js
+++ gate-opener/src/main.js
@@ -2,19 +2,23 @@ const express = require("express");
 const exec = require("await-exec");
 
 const PORT = process.env.PORT || 8000;
-const CMD = `sudo ./sendiq -s 250000 -f "433.9"e6 -t u8 -i record.iq >/dev/null 2>/dev/null`;
+const RECORDING_FILE = "record.iq";
+const FREQUENCY = "433.9";
+const CMD = `sudo ./sendiq -s 250000 -f "${FREQUENCY}"e6 -t u8 -i ${RECORDING_FILE} >/dev/null 2>/dev/null`;
 
 var app = express();
 
 app.get("/gate", async (req, res, next) => {
     try {
-        await exec(CMD)
+        console.log(`Running command: ${CMD}`);
+        await exec(CMD);
         res.status(200).json({
             success: true,
             msg: "Radio signal transmitted successfully."
         });
     }
     catch(error) {
+        console.error(error);
         res.status(500).json({
             success: false,
             msg: "An error occured while running sendiq command.",
